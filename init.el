3f1a2bf3385f7cd7ac1c4066e3dd58415f64d7dd
ongwinston <43237906+ongwinston@users.noreply.github.com>
2020-08-27 20:09:43 +1000

    Add files via upload

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
 ;; (load-file (concat user-emacs-directory "/cedet/cedet-devel-load.el"))
 ;; (add-to-list 'load-path (concat user-emacs-directory "cedet/contrib"))
 ;; (load-file (concat user-emacs-directory "cedet/contrib/cedet-contrib-load.el"))
-
  (require 'semantic)
  (semantic-mode 1)
  (global-semantic-stickyfunc-mode 1)
-
 ;; global variables
 (setq
  inhibit-startup-screen t
@@ -16,21 +14,22 @@
  show-paren-delay 0.5
  use-package-always-ensure t
  sentence-end-double-space nil)
-
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq visible-bell 1)
+(toggle-truncate-lines t)
 ;; buffer local variables
 (setq-default
  indent-tabs-mode nil
  tab-width 4
- c-basic-offset 4)
-
+ c-basic-offset 4
+)
 ;; modes
 (electric-indent-mode 0)
 (windmove-default-keybindings 'meta)
-
 ;; global keybindings
 (global-unset-key (kbd "C-z"))
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
 ;; the package manager
 (require 'package)
 (setq
@@ -39,7 +38,6 @@
                     ("melpa" . "http://melpa.org/packages/")
                     ("melpa-stable" . "http://stable.melpa.org/packages/"))
  package-archive-priorities '(("melpa-stable" . 1)))
-
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents)
@@ -50,6 +48,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(package-selected-packages (quote (vdiff yaml-mode ensime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -58,5 +57,6 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'scroll-left 'disabled nil)
-
-(load-theme `tango-dark)
+;;(load-theme `tango-dark)
+;;(load-theme `deeper-blue)
+(setq verilog-auto-newline nil)
